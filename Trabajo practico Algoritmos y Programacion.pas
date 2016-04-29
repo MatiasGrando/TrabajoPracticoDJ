cd98606d64c6ac19b47d4fe7969a0eab249fe1e6
MatiasGrando <matias.grando@hotmail.com>
2016-04-29 16:03:22 -0300

    Update Trabajo practico Algoritmos y Programacion.pas

--- Trabajo practico Algoritmos y Programacion.pas
+++ Trabajo practico Algoritmos y Programacion.pas
@@ -34,6 +34,7 @@ Type
 	tmTemasAsignados = array [tIndiceDj, tIndiceTemasPorDj] of tTitulo;
 	tvCantTemasPorDj= array [tIndiceTemasPorDj] of tBaseCantTemasPorDj;
 	tCantDjs=0..MaxDj;
+	tvAcumTiempo= array [1..maxdj] of word;
 Var
 	vTemasOficiales: tvTemasOficiales;
 	vDuracion: tvDuracion;
@@ -41,14 +42,128 @@ Var
 	mTemasAsignados: tmTemasAsignados;
 	vCantTemasPorDj: tvCantTemasPorDj; { Vector Paralelo a la matriz de temas Djs indica Maximo Logico (numerico)}
 	Titulo: tTitulo;
-    DuracionEnSeg: tBaseDuracion;
-    CantDjs:tCantDjs; {funciona como ml (cuantos djs hay?) (numerico)}
+        DuracionEnSeg: tBaseDuracion;
+        CantDjs:tCantDjs; {funciona como ml (cuantos djs hay?) (numerico)}
+        vAcumTiempo:tvacumtiempo;
 
 
 
 
 
+{---------------------Procedimiento Dj Toca Mas Tiempo------------------}
 
+
+
+procedure PasarAFormatoValido(var maxaux:word);
+{aca paso el formato segundos a formato hh:mm:ss}
+
+var
+        staux:string[8];
+        st0:string[1];
+        st1, st2, st3:string[2];
+        mins, seg, horas: word;
+        car: char;
+begin
+        staux:='';
+        st0:='0';
+        horas:=maxaux div 3600;
+        mins:=(maxaux mod 3600) div 60;
+        seg:=maxaux-((horas*3600)+(mins*60));
+        str(horas,st1);
+        if horas<10 then
+                insert(st1,st0,1);
+        str(mins,st2);
+        if mins<10 then
+                insert(st2,st0,1);
+        str(seg,st3);
+        if seg<10 then
+                insert(st3,st0,1);
+        staux:=concat(st1,':',st2,':',st3);
+        writeln(staux);
+
+end;
+
+
+procedure EscribirDjMaxToca(vAcumTiempo:tvAcumTiempo;max:word;vDj:tvDj);
+
+{aca lo que hace es escribir el o los djs que mas tocan junto con el tiempo, pero ese tiempo lo tengo que pasar a formato hh:mm:ss,
+por eso puse la funcion esa pasaraformatovalido}
+
+var
+i:byte;
+maxaux:word;
+
+begin
+        maxaux:=max;
+        for i:=1 to maxdj do
+                if vAcumTiempo[i]=max then
+                        begin
+                                write(vdj[i],': ');
+                                PasarAFormatoValido(maxaux);
+                        end;
+end;
+
+
+
+procedure DjMaxToca(vAcumTiempo:tvAcumTiempo;vDj:tvDj);
+
+{aca busco cual es el dj que mas toca, comparo en el vector vacumtiempo cual es el mayor y ahi saco la posicion del dj que mas toca,
+puede ser mas de uno}
+
+var
+        i:byte;
+        max:word;
+        cont:byte;
+
+begin
+        cont:=0;
+        max:=0;
+        for i:=1 to maxdj do
+                if vAcumTiempo[i]>max then
+                        max:=vAcumTiempo[i]; {busco duracion maxima en seg}
+        for i:=1 to maxdj do
+                if vAcumTiempo[i]=max then {contador para ver si es 1 o + djs}
+                        cont:=cont+1;
+        if cont=1 then
+                begin
+                writeln('El Dj que mas tiempo toca es:');
+                EscribirDjMaxToca(vAcumTiempo,max,vDj);
+                end;
+        if cont<>1 then begin
+                writeln('Los Dj que mas tocan son:');
+                EscribirDjMaxToca(vacumtiempo,max,vdj);
+                end;
+end;
+
+
+procedure BuscoMaximo (var mTemasAsignados:tmTemasAsignados;var vAcumTiempo:tvAcumTiempo; vDj:tvDj; vDuracion:tvDuracion; vTemasOficiales:tvTemasOficiales);
+
+{La matriz mTemasAsignados no se va a modificar pero se pasa por referencia porque es muy pesado, entonces ocupa mucha memoria}
+{en este procedure la idea es sumar en un vector vduracion la duracion total por cada dj, la duracion queda en segundos, mas adelante la convierto a hh:mm:ss}
+
+var
+i,j,z:byte;
+tacum,num:word;
+
+begin
+
+      for i:=1 to maxdj do
+      begin
+        j:=1;
+        tacum:=0;
+        repeat
+        for z:=1 to maxtemasoficiales do
+                if mTemasAsignados[i,j]=vTemasOficiales[z] then
+                        tacum:=tacum+vDuracion[z];
+        j:=j+1
+        until (mTemasAsignados[i,j]=parar) or (j=maxtemaspordj);
+      {aca busco el tema de la matriz en el vector de los 200 temas y con el
+      vector paralelo vduracion voy sumando las duraciones y lo asigno al
+      vector vacumtiempo donde cada espacio es la duracion total del dj}
+        vAcumTiempo[i]:=tacum
+        end;
+        djmaxtoca(vAcumTiempo,vDj);
+end;
 
 
 
@@ -292,7 +407,7 @@ Begin
 	clrscr; 
 	CargarListaOficial(vtemasOficiales,vDuracion);
 	CargarInfoDJs(vDj,vCantTemasPorDj,mTemasAsignados,vTemasOficiales,CantDjs);
-
-	{Esta hecho lo de cargar las listas de temas y de Djs}
+	BuscoMaximo (mTemasAsignados, vAcumTiempo, vDj,vDuracion,vTemasOficiales);
+	{Esta hecho lo de cargar las listas de temas y de Djs y el punto 3 de buscar el dj que toca mas tiempo}
 
 End.
